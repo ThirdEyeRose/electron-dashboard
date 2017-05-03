113acfa39b4e7fd2c14b06a8c8cf1a4b661a2a85
Pat East <that-pat@github.com>
2017-05-03 14:09:50 -0500

    Add some comments explaining what I'm doing

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 $(function() {
 
+  // Import access to electron's UI
   const electron = require('electron');
   const shell = electron.shell;
 
+  // Make sure that all job board links are opened in regular browser
   $('.job-board').on('click', 'a', function (e) {
     e.preventDefault();
     shell.openExternal(e.target.href);
